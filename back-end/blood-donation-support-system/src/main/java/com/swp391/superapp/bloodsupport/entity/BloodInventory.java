619150f81d013549ff96f478710a4fbbbe9d87b8
an7-seven <vietann.0409@gmail.com>
2025-06-02 14:07:51 +0700

    update-project

---
+++ back-end/blood-donation-support-system/src/main/java/com/swp391/superapp/bloodsupport/entity/BloodInventory.java
@@ -0,0 +1,12 @@
+package com.swp391.superapp.bloodsupport.entity;
+
+public class BloodInventory {
+    private int bloodCode;
+    private String blood;
+    private String rh;
+    private String component;
+    private boolean isRareBood;
+    private int quantity;
+
+    
+}
---
+++ back-end/blood-donation-support-system/src/main/java/com/swp391/superapp/bloodsupport/entity/Profile.java
@@ -0,0 +1,17 @@
+package com.swp391.superapp.bloodsupport.entity;
+
+import java.time.LocalDate;
+
+
+public class Profile {
+    private String accountId;
+    private String name;
+    private String phone;
+    private LocalDate dob; //xem lại
+    private boolean gender;
+    private String address;
+    private Role role;
+    private int numberOfBloodDonation;
+
+
+}
