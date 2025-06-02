b56107deceece6bbe0bcdd87c3a977213401fe5c
Hồ Trần Quân <116471791+ShiroSs00@users.noreply.github.com>
2025-06-02 20:42:17 +0700

    Delete front-end directory

--- front-end/src/components/sections/index.ts
+++
@@ -1,7 +0,0 @@
-export { default as Header } from './header';
-export { default as Footer } from './footer';
-export { default as Hero } from './hero';
-export { default as About } from './about';
-export { default as Services } from './services';
-export { default as Contact } from './contact';
-export { default as ProcessSteps } from './processsteps';
