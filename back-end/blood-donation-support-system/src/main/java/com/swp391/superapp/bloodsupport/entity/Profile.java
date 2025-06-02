e967ca50d5d5b7f5548b5f70d349628605027f86
an7-seven <vietann.0409@gmail.com>
2025-06-02 14:38:55 +0700

    update comment

--- back-end/blood-donation-support-system/src/main/java/com/swp391/superapp/bloodsupport/entity/Profile.java
+++ back-end/blood-donation-support-system/src/main/java/com/swp391/superapp/bloodsupport/entity/Profile.java
@@ -1,7 +1,7 @@
 /* File: Account.java
  * Author: SE184889 - Nguyễn Trần Việt An (AnNTV)
  * Created on: 02-06-2025
- * Purpose: Pepresnets the Account entity used for user authentication and profile manage
+ * Purpose: Pepresnets the Profile entity used for user authentication and profile manage
  *
  * Change Log:
  * [02-06-2025] - Created by: AnNTV
